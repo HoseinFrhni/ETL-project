de8f9fd85f1d559bc55225e87e35c54187d7b714
HoseinFrhni <113766368+HoseinFrhni@users.noreply.github.com>
2025-11-11 13:01:12 -0800

    Start cleaning 100000 customer data in transform.py

--- etl/transform.py
+++ etl/transform.py
@@ -1,20 +1,33 @@
 import pandas as pd
-from typing import List,Dict
+from typing import List, Dict
+
+from sqlalchemy.util import column_set
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 
-def normalize_market_items(items:List[Dict]) ->pd.DataFrame:
-    rows = []
-    for item in items:
-        rows.append({
-            "id":item.get("id"),
-            "symbol":item.get("symbol"),
-            "name":item.get("name"),
-            "currency_price":item.get("currency_price"),
-            "market_cap":item.get("market_cap"),
-            "total_volume":item.get("total_volume"),
-            "price_change_24h":item.get("price_change_24h"),
-            "price_change_pct_24h":item.get("price_change_pct_24h"),
-            "last_updated":item.get("last_updated")
-        })
-    df = pd.DataFrame(rows)
-    return df
+def normalize_market_items(csv_file_path: str):
+    coin_data_frame = pd.read_csv(csv_file_path)
+    coin_data_frame = coin_data_frame.drop_duplicates()
+    coin_data_frame = coin_data_frame.drop(columns="Phone 2")
+    # clean last name column use strip with somthing like regex :)
+    coin_data_frame["Last Name"] = coin_data_frame["Last Name"].str.strip("123./_@&,")
+    # clean Phone 1 column with replace method for delete characters
+    coin_data_frame["Phone 1"] = coin_data_frame["Phone 1"].str.replace('[^a-zA-Z0-9]', '', regex=True)
+    # transform column to str
+    coin_data_frame["Phone 1"] = coin_data_frame["Phone 1"].apply(lambda x: str(x))
+    # add '-' between each 3 characters
+    coin_data_frame["Phone 1"] = coin_data_frame["Phone 1"].apply(lambda x: x[0:3] + '-' + x[3:6] + '-' + x[6:10])
+    # in continue we clean records with NAN & nan & na & NA & NA-- & na-- ,...
+    coin_data_frame["Phone 1"] = coin_data_frame["Phone 1"].str.replace('na--', '', regex=True)
+    coin_data_frame["Phone 1"] = coin_data_frame["Phone 1"].str.replace('Na--', '', regex=True)
+    coin_data_frame["Phone 1"] = coin_data_frame["Phone 1"].str.replace('NA--', '', regex=True)
+    coin_data_frame["Phone 1"] = coin_data_frame["Phone 1"].str.replace('nA--', '', regex=True)
+    coin_data_frame["Phone 1"] = coin_data_frame["Phone 1"].str.replace('Nan', '', regex=True)
+    coin_data_frame["Phone 1"] = coin_data_frame["Phone 1"].str.replace('NaN', '', regex=True)
+
+    print(coin_data_frame.head(50))
+
+
+normalize_market_items(r"E:\ETL-project\data\raw\customers-100000.csv")
